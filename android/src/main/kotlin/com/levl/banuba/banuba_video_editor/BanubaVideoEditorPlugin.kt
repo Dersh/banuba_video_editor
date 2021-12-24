321520b85ce23609eb585373a487501fc90385c9
Andrey Dershevich <adershevich@ego-cms.com>
2021-12-24 17:41:06 +0300

    android code

--- android/src/main/kotlin/com/levl/banuba/banuba_video_editor/BanubaVideoEditorPlugin.kt
+++ android/src/main/kotlin/com/levl/banuba/banuba_video_editor/BanubaVideoEditorPlugin.kt
@@ -1,35 +1,108 @@
 package com.levl.banuba.banuba_video_editor
 
-import androidx.annotation.NonNull
+import android.app.Activity
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 
+import androidx.annotation.NonNull
+import com.banuba.sdk.ve.data.EXTRA_EXPORTED_SUCCESS
+import com.banuba.sdk.ve.data.ExportResult
+import com.banuba.sdk.ve.flow.VideoCreationActivity
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.PluginRegistry
+
 
 /** BanubaVideoEditorPlugin */
-class BanubaVideoEditorPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class BanubaVideoEditorPlugin : FlutterPlugin, BanubaVideoEditorPluginApi.BanubaVideoEditorApi,
+    ActivityAware, PluginRegistry.ActivityResultListener {
+    private var activity: Activity? = null
+    private var pendingResult: BanubaVideoEditorPluginApi.Result<BanubaVideoEditorPluginApi.VideoEditResult>? =
+        null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "banuba_video_editor")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    companion object {
+        private const val VIDEO_EDITOR_REQUEST_CODE = 34535
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        BanubaVideoEditorPluginApi.BanubaVideoEditorApi.setup(
+            flutterPluginBinding.binaryMessenger,
+            this
+        )
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        BanubaVideoEditorPluginApi.BanubaVideoEditorApi.setup(binding.binaryMessenger, null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+        binding.addActivityResultListener(this)
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun startEditorFromCamera(result: BanubaVideoEditorPluginApi.Result<BanubaVideoEditorPluginApi.VideoEditResult>?) {
+        pendingResult = result
+        activity?.let {
+            it.startActivityForResult(
+                VideoCreationActivity.startFromCamera(
+                    context = it
+                ), VIDEO_EDITOR_REQUEST_CODE
+            )
+        } ?: run {
+            finishWithError(NoActivityException())
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?): Boolean {
+        if (pendingResult == null) {
+            return false
+        }
+        if (resultCode == RESULT_OK && requestCode == VIDEO_EDITOR_REQUEST_CODE) {
+            val exportedVideoResult =
+                intent?.getParcelableExtra(EXTRA_EXPORTED_SUCCESS) as? ExportResult.Success
+
+            finishWithSuccess(exportedVideoResult.toString())
+            return true
+        } else if (resultCode == RESULT_CANCELED && requestCode == VIDEO_EDITOR_REQUEST_CODE) {
+            finishWithSuccess(null)
+            return true
+        }
+        return false
+    }
+
+    private fun finishWithError(error: Throwable) {
+        if (pendingResult == null) {
+            return
+        }
+        pendingResult?.error(error)
+        clearPendingResult()
+    }
+
+    private fun finishWithSuccess(videoPath: String?) {
+        val result = BanubaVideoEditorPluginApi.VideoEditResult()
+        result.filepath = videoPath
+        pendingResult?.success(result)
+        clearPendingResult()
+    }
+
+    private fun clearPendingResult() {
+        pendingResult = null
+    }
 }
+
+
+class NoActivityException : Exception("video_editor requires a foreground activity")
--- test/banuba_video_editor_test.dart
+++ test/banuba_video_editor_test.dart
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await BanubaVideoEditor.platformVersion, '42');
+    
   });
 }
