27022e45a3605a95e3ef3fe05d3b77c5327d03ba
Andrey Dershevich <adershevich@ego-cms.com>
2021-12-28 16:48:39 +0300

    method renamed

--- lib/banuba_video_editor.dart
+++ lib/banuba_video_editor.dart
@@ -4,6 +4,6 @@ import 'banuba_video_editor_api.dart';
 
 class BanubaVideoEditor {
   static late final BanubaVideoEditorApi _api = BanubaVideoEditorApi();
-  static Future<VideoEditResult> selectAndEditVideo() =>
+  static Future<VideoEditResult> startEditorFromCamera() =>
       _api.startEditorFromCamera();
 }
